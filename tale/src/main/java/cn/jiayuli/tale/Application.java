9edfbe265b8e923b32a10f9149b82a4a4fd54165
jiayu <1214942360@qq.com>
2018-01-31 18:00:38 +0800

    添加Tale的maven工程

---
+++ tale/src/main/java/cn/jiayuli/tale/Application.java
@@ -0,0 +1,17 @@
+package cn.jiayuli.tale;
+
+import com.blade.Blade;
+
+/**
+ * @author jiayu
+ * @description Tale启动类
+ * @date 2018/1/31 16:16
+ */
+public class Application {
+
+    public static void main (String[] args) throws Exception {
+
+        Blade blade = Blade.me();
+
+    }
+}
---
+++ tale/src/main/java/cn/jiayuli/tale/init/TaleLoader.java
@@ -0,0 +1,9 @@
+package cn.jiayuli.tale.init;
+
+/**
+ * @author jiayu
+ * @description
+ * @date 2018/1/31 16:23
+ */
+public final class TaleLoader {
+}
