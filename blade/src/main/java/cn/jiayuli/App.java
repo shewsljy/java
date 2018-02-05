b3307d4051c7fb57db01ea403892a71827c4f573
jiayu <1214942360@qq.com>
2018-02-05 16:44:06 +0800

    添加blade-mvc

---
+++ blade/src/main/java/cn/jiayuli/App.java
@@ -0,0 +1,13 @@
+package cn.jiayuli;
+
+import com.blade.Blade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        Blade.me().start(App.class);
+    }
+}
