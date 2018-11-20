dea0b50a03887b29b19e80464d73fdbf4b6b48f6
李家宇 <shewsljy@163.com>
2018-11-20 10:56:09 +0800

    springboot mybatis 多mapper改造完毕

--- myhome/src/test/java/cn/jiayuli/myhome/MyhomeApplicationTests.java
+++ myhome/src/test/java/cn/jiayuli/myhome/MyhomeApplicationTests.java
@@ -1,8 +1,11 @@
 package cn.jiayuli.myhome;
 
+import cn.jiayuli.myhome.dao.UserInfoMapper;
 import cn.jiayuli.myhome.demo.dao.DemoUserInfoMapper;
 import cn.jiayuli.myhome.demo.model.DemoUserInfo;
 import cn.jiayuli.myhome.demo.model.DemoUserInfoExample;
+import cn.jiayuli.myhome.model.UserInfo;
+import cn.jiayuli.myhome.model.UserInfoExample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +21,14 @@ public class MyhomeApplicationTests {
 
     @Resource DemoUserInfoMapper demoUserInfoMapper;
 
+    @Resource UserInfoMapper userInfoMapper;
+
     @Test
     public void contextLoads() {
     }
 
     @Test
-    public void mapperTest(){
+    public void demoUserInfoTest() {
         List<DemoUserInfo> demoUserInfoList = new ArrayList<DemoUserInfo>();
         DemoUserInfoExample example = new DemoUserInfoExample();
         DemoUserInfoExample.Criteria criteria = example.createCriteria();
@@ -33,6 +38,20 @@ public class MyhomeApplicationTests {
         demoUserInfoList = demoUserInfoMapper.selectByExample(example);
         for (DemoUserInfo demoUserInfo : demoUserInfoList) {
             System.out.println(demoUserInfo.toString());
+        }
+    }
+
+    @Test
+    public void userInfoTest() {
+        List<UserInfo> userInfoList = new ArrayList<UserInfo>();
+        UserInfoExample example = new UserInfoExample();
+        UserInfoExample.Criteria criteria = example.createCriteria();
+        criteria.andIdIsNotNull();
+        criteria.andPassWordIsNotNull();
+        criteria.andUpdateUserIsNull();
+        userInfoList = userInfoMapper.selectByExample(example);
+        for (UserInfo userInfo : userInfoList) {
+            System.out.println(userInfo.toString());
         }
     }
 }
