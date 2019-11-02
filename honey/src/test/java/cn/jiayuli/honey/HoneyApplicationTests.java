b8d276388111d281213c2b57de6793a7d048f2b0
李家宇 <shewsljy@163.com>
2019-11-02 12:00:53 +0800

    homey工程搭建

---
+++ honey/src/test/java/cn/jiayuli/honey/HoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jiayuli.honey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
