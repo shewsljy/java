44f7946be80c68b4edfe68d492c7fa32a958d1c8
jiayu <1214942360@qq.com>
2021-02-23 10:40:11 +0800

    allsome init

---
+++ allsome/src/main/java/cn/jiayuli/allsome/AllsomeApplication.java
@@ -0,0 +1,13 @@
+package cn.jiayuli.allsome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllsomeApplication.class, args);
+    }
+
+}
---
+++ allsome/src/test/java/cn/jiayuli/allsome/AllsomeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jiayuli.allsome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllsomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
